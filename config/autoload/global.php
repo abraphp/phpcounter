87bb3e8a41a83cd88dfff8c48bdfe522ac8c6d37
Elton Minetto <eminetto@gmail.com>
2013-07-05 12:32:10 -0300

    Configuração do Doctrine

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -4,6 +4,20 @@ return array(
         'driver' => 'Pdo',
         'dsn'    => 'mysql:dbname=phpcounter;host=localhost'
     ),
+    'doctrine' => array(
+        'connection' => array(
+            'driver'   => 'pdo_mysql',
+            'host'     => 'localhost',
+            'port'     => '3306',
+            'user'     => 'root',
+            'password' => '',
+            'dbname'   => 'phpcounter'
+        ),
+        'driver' => array(
+            'cache' => 'Doctrine\Common\Cache\ArrayCache',
+            'paths' => array(__DIR__ . '/../module/Admin/src/Admin/Model', __DIR__ . '/../module/Application/src/Application/Model')
+        ),
+    ),
     'service_manager' => array(
         'factories' => array(
             'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
